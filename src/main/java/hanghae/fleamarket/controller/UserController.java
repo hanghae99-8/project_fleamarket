3f48e1d2c821a083701b556a3b0ef7407c0b8bb4
혜민\forbe <hyeomin9326@gmail.com>
2023-01-13 22:27:13 +0900

    #2 회원가입 (@RequestBody추가)

---
+++ src/main/java/hanghae/fleamarket/controller/UserController.java
@@ -0,0 +1,77 @@
+package hanghae.fleamarket.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hanghae.fleamarket.dto.LoginRequestDto;
+import hanghae.fleamarket.dto.SignupRequestDto;
+import hanghae.fleamarket.jwt.JwtUtil;
+import hanghae.fleamarket.service.KakaoService;
+import hanghae.fleamarket.service.UserService;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+    private final KakaoService kakaoService;
+
+    @GetMapping("/signup")
+    public ModelAndView signupPage() {
+        return new ModelAndView("signup");
+    }
+
+    @GetMapping("/login-page")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login");
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequestDto signupRequestDto) {
+
+        log.info("username = {}", signupRequestDto.getUsername());
+        log.info("password = {}", signupRequestDto.getPassword());
+
+        userService.signup(signupRequestDto);
+        return "redirect:/api/user/login-page";
+    }
+
+    @ResponseBody
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        userService.login(loginRequestDto, response);
+        return "success";
+    }
+
+    @GetMapping("/kakao/callback")
+    public ModelAndView kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        // code: 카카오 서버로부터 받은 인가 코드
+        String createToken = kakaoService.kakaoLogin(code, response);
+
+        // Cookie 생성 및 직접 브라우저에 Set, 서버에서 쿠키를 쿠키저장소에 넣어줌
+        //키값                              밸류값 , substring(bearer과 공백을 삭제)
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/forbidden")
+    public ModelAndView getForbidden() {
+        return new ModelAndView("forbidden");
+    }
+
+    @PostMapping("/forbidden")
+    public ModelAndView postForbidden() {
+        return new ModelAndView("forbidden");
+    }
+
+}
