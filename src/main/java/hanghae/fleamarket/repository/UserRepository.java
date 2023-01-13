78c6d2accf1cfceff2bafd5cd69b3cc1c47c1ace
anwonseok <anwonseok72@gmail.com>
2023-01-13 21:26:28 +0900

    fis

---
+++ src/main/java/hanghae/fleamarket/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package hanghae.fleamarket.repository;
+
+import hanghae.fleamarket.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByKakaoId(Long id);
+    Optional<User> findByEmail(String email);
+}
