76dcae6a0e926f48cc0a80f1aea8321ef7621ed7
anwonseok <anwonseok72@gmail.com>
2023-01-19 17:00:44 +0900

    구글/카카오로그인 리턴값 수정

--- src/main/java/hanghae/fleamarket/controller/UserController.java
+++ src/main/java/hanghae/fleamarket/controller/UserController.java
@@ -68,10 +68,11 @@ public class UserController {
 
     //카카오 로그인
     @GetMapping("/kakao/callback")
-    public void kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         // code: 카카오 서버로부터 받은 인가 코드
-        kakaoService.kakaoLogin(code, response);
+        String jwt = kakaoService.kakaoLogin(code, response);
 
+        return jwt;
 //        if (createToken != null) {
             // Cookie 생성 및 직접 브라우저에 Set, 서버에서 쿠키를 쿠키저장소에 넣어줌
             //키값                              밸류값 , substring(bearer과 공백을 삭제)
@@ -103,12 +104,10 @@ public class UserController {
 
     //구글 로그인 redirect
     @GetMapping(value = "/google/login/redirect")
-    public void redirectGoogleLogin(@RequestParam(value = "code") String authCode, HttpServletResponse response) {
+    public String redirectGoogleLogin(@RequestParam(value = "code") String authCode, HttpServletResponse response) {
         String jwt = googleService.redirectGoogleLogin(authCode);
 
-        if (jwt != null) {
-            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwt.substring(7));
-        }
+       return jwt.substring(7);
     }
 
     //접근 제한
--- src/main/java/hanghae/fleamarket/service/KakaoService.java
+++ src/main/java/hanghae/fleamarket/service/KakaoService.java
@@ -10,6 +10,7 @@ import hanghae.fleamarket.jwt.JwtUtil;
 import hanghae.fleamarket.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -31,7 +32,7 @@ public class KakaoService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
-    public void kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
+    public String kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
         // 1. 카카오에 "인증 코드"로 "액세스 토큰" 요청
         String accessToken = getToken(code);
 
@@ -43,9 +44,8 @@ public class KakaoService {
 
         // 4. JWT 토큰 반환
         String createToken =  jwtUtil.createToken(kakaoUser.getUsername(), kakaoUser.getRole());
-        //jwt토큰을 만들어 클라에게 전달(클라에서 직접 쿠키에 저장)
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-//        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, createToken);
+
+        return createToken.substring(7);
     }
 
     // 1. "인가 코드"로 "액세스 토큰" 요청
