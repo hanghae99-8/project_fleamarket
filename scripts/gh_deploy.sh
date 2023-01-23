1efa1b6a1767bc4d3246023dae8d3dc7baa5ff07
혜민\forbe <hyeomin9326@gmail.com>
2023-01-23 19:50:38 +0900

    오타수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="project-fleamarket"
-JAR_PATH="/home/ubuntu/project-fleamarket/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/project_fleamarket/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
